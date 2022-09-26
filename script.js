33f464b2c931356ee69ca5e2f1e4198205142f41
Carolina Peraza <carolina.peranie@gmail.com>
2022-09-26 10:52:22 -0500

    feat: uso de script externo

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("saoko papi, saokooo");
+document.getElementById ("reeeed(:").style.color = "grey"
+function color (){
+  console.log("pulso el boton de generation");
+  document.getElementById ("reeeed(:").style.color = "blue"; }
